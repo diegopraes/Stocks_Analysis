fc68881b3e081832027f73308bde8671c0352a91
Diego Praes <37059524+diegopraes@users.noreply.github.com>
2018-03-06 09:41:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 # Sotck Analysis
+# =============================================================================
+# Analysis of stocks data: data plot, rolling mean, returns, correlation.
+# Database connection pool and table query and construction.
+# Realtime data of stocks values and shares to buy.
+# =============================================================================
 """
 Created on Sun Mar  4 17:50:47 2018   @author: DiegoPraes
 """
-
 # BOVESPA:
 # 'BBAS3', 'ABEV3', 'PETR3', 'ITUB4', 'USIM5','VALE3', 'GGBR4', 'EMBR3', 'ELET3',
 # 'SUZB5', 'RENT3', 'NATU3', 'MRVE3'
@@ -10,25 +14,26 @@ Created on Sun Mar  4 17:50:47 2018   @author: DiegoPraes
 # DOW JONES:
 # 'XOM', 'AAPL', 'MCD', 'KO', 'CVX', 'BA', 'CAT', 'IBM', 'MMM', 'WMT', 'V', 'NKE'
 
-
-
-user_input = input("\n 'r' real time data\n\n 'a' to correlation analysis\n\n 'd' to database access\n\n 'q' to quit\n\n>>> ")
-
-while user_input not in ['r', 'a', 'd', 'q']:
-    user_input = input("\n 'r' real time data\n\n 'a' to correlation analysis\n\n 'd' to database access\n\n 'q' to quit\n\n>>> ")
-
-if user_input != 'q':   
-    if user_input == 'r':
-        import realtime
+def menu():
+    print('==================================================================')
+    print('Stocks Analysis Python')
+    print('==================================================================')
+    print('\nMenu():')
     
-    elif user_input == 'a':
-        import analysis
+    user_input = input(" 'r' real time data\n\n 'a' to correlation analysis\n\n 'd' to database access\n\n 'q' to quit\n\n>>> ")
     
-    elif user_input == 'd':
-        from database import database_access
-        database_access()
-
-
-# my_dict = dict('start_date': {'open': float, 'high': float, 'low': float, 'close': float},'end_date': {'open': float, 'high': float, 'low': float, 'close': float})
+    while user_input not in ['r', 'a', 'd', 'q']:
+        user_input = input("\n 'r' real time data\n\n 'a' to correlation analysis\n\n 'd' to database access\n\n 'q' to quit\n\n>>> ")
+    
+    if user_input != 'q':   
+        if user_input == 'r':
+            import realtime
         
+        elif user_input == 'a':
+            import analysis
         
+        elif user_input == 'd':
+            from database import database_access
+            database_access()
+            
+menu()
