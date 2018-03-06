c9476221f1f94e931407724b773d4c0e5747785c
Diego Praes <37059524+diegopraes@users.noreply.github.com>
2018-03-06 09:41:29 -0300

    Update realtime.py

--- realtime.py
+++ realtime.py
@@ -10,24 +10,20 @@ bov = ['BBAS3', 'ABEV3', 'PETR3', 'ITUB4', 'USIM5','VALE3', 'GGBR4', 'EMBR3', 'E
 dj = ['XOM', 'AAPL', 'MCD', 'KO', 'CVX', 'BA', 'CAT', 'IBM', 'MMM', 'WMT', 'V', 'NKE']
 
 
-start = input('Enter the initial date [YYYY, MM, DD]:\n\n>>> ')
-end = input('Enter the final date [YYYY, MM, DD]:\n\n>>> ')
-st = start.split(',')
-nd = end.split(',')
-start_date = datetime.datetime(int(st[0]), int(st[1]), int(st[2]))
-end_date = datetime.datetime(int(nd[0]), int(nd[1]), int(nd[2]))
+date = input('Enter the date [YYYY, MM, DD]:\n\n>>> ')
+dt = date.split(',')
+date = datetime.datetime(int(dt[0]), int(dt[1]), int(dt[2]))
 
 portifolio = float(input('Portifolio:\n\n>>> '))
 
 
 class Data:
-    def __init__(self, index, start, end):
+    def __init__(self, index, date):
         self.index = index
-        self.start = start
-        self.end = end
+        self.date = date
         
     def get(self):
-        return web.DataReader(self.index, 'google', self.start, self.end)    
+        return web.DataReader(self.index, 'google', self.date)    
 
     
 user_input = input('\n Enter: \n\n [bov] for Bovespa \n\n [dj] for Dow Jones \n\n>>> ' )
@@ -42,14 +38,16 @@ elif user_input == 'dj':
 
 for index in index_list:
     try:
-        print('\n\nIndex: {}\n'.format(index))
-        print(Data.get(Data(index, start_date, end_date)))
-        d = Data.get(Data(index, start_date, end_date))
-        print('\nShares to buy [start date]:\n')
-        print(portifolio/100/(float(d['Close'][0]) - float(d['Open'][0])))
-        print('\n')
-        print('Shares to buy [end date]:\n')
-        print(portifolio/100/(float(d['Close'][1]) - float(d['Open'][1])))
+        print('\n\nIndex: {}\n'.format(index))        
+        d = Data.get(Data(index, date))
+        print(d)
+        var = float(d['Close'][0]) - float(d['Open'][0])
+        s = portifolio/100/var
+        shares = float(portifolio)/float(d['Open'][0])
+        print('\nDaily variation: {:.2f}'.format(var))
+        print('\nBuy {:.2f} shares to 1% risk'.format(s))
+        print('\nMax shares to buy (using total portifolio): {:.2f}'.format(shares))
+        print('\nProfit: {:.2f}'.format(shares*var))
         print('\n')
     except:
         print('\n\nNot possible to get data for {}\n'.format(index))
