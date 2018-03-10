56fc8665d18620d8a7c3871f170ac8b4d8a38262
Diego Praes <37059524+diegopraes@users.noreply.github.com>
2018-03-10 14:32:08 -0300

    Update Inteligent.py

--- Inteligent.py
+++ Inteligent.py
@@ -1,10 +1,14 @@
 import datetime
 from pandas_datareader import data as web
 import numpy as np
+from matplotlib.finance import candlestick2_ohlc
+import matplotlib.pyplot as plt
+import pandas as pd
+
 
 bov = ['BBAS3', 'ABEV3', 'PETR3', 'ITUB4', 'RADL3', 'USIM5', 'VALE3', 'GGBR4', 'EMBR3', 'ELET3', 'RENT3', 'NATU3', 'CMIG4', 'GOAU4', 'BTOW3', 'AZUL4', 'CCRO3', 'LREN3', 'IRBR3', 'UGPA3', 'QUAL3', 'BRDT3', 'BBSE3', 'CIEL3', 'BRAP4', 'CSNA3', 'ESTC3', 'SLCE3', 'ELET3', 'LIGT3', 'OIBR3', 'BRML3', 'ENGI11', 'EQTL3', 'DMMO3', 'CVCB3', 'JBSS3', 'VIVT4', 'KLBN11', 'IGTA3', 'SULA11', 'TIMP3', 'CRFB3', 'RAIL3', 'MULT3', 'GOLL4', 'BRKM5', 'SBSP3', 'VVAR11', 'BRSR6', 'BBDC3', 'SANB11', 'FLRY3', 'PCAR4', 'CYRE3', 'MDIA3', 'WEGE3', 'CSAN3', 'CPFE3', 'MPLU3', 'LAME3', 'ECOR3', 'EGIE3', 'PSSA3', 'CSMG3', 'ARZZ3', 'HGTX3', 'TOTS3', 'PARD3']
-
-dj = ['XOM', 'AAPL', 'MCD', 'KO', 'CVX', 'BA', 'CAT', 'IBM', 'MMM', 'WMT', 'V', 'NKE']
+    
+dj = ['XOM', 'AAPL', 'MCD', 'KO', 'CVX', 'BA', 'CAT', 'IBM', 'MMM', 'WMT', 'V', 'NKE']      
 
 user_input = input('\n Enter: \n\n [bov] for Bovespa \n\n [dj] for Dow Jones \n\n>>> ' )
 while user_input not in ['bov', 'dj']:
@@ -12,27 +16,27 @@ while user_input not in ['bov', 'dj']:
 if user_input == 'bov':
     index_list = bov    
 elif user_input == 'dj':
-    index_list = dj   
+    index_list = dj 
+    
+if user_input == 'bov':
+    index_list = bov
+elif user_input =='dj':
+    index_list = dj 
 
 portifolio = float(input('\nEnter the Portifolio Value:\n\n>>> '))
-
+    
 start = input('Enter the initial date [YYYY, MM, DD]:\n\n>>> ')
 dt = start.split(',')    
 
 end = input('Enter the end date [YYYY, MM, DD]:\n\n>>> ')
 st = end.split(',')
-        
-if user_input == 'bov':
-    index_list = bov
-elif user_input =='dj':
-    index_list = dj    
 
 selection = []
 possible_to_buy = []
 up_trend = []
 signal = []
-
-for i in index_list:
+    
+for i in index_list:         
     
     start = datetime.datetime(int(dt[0]), int(dt[1]), int(dt[2]))
     end = datetime.datetime(int(st[0]), int(st[1]), int(st[2]))
@@ -133,24 +137,74 @@ for i in index_list:
                     print('Error')
                     continue                
 
-print('\n\n\nSelection: {}'.format(selection))
-print('\nPossible to buy: {}'.format(possible_to_buy))
-print('\nUp Trend: {}'.format(up_trend))
-print('\nSignal: {}'.format(signal))
+print('\n\n\nSelection: {}'.format(selection))              # up, +100 shares, profit > risk
+print('\nPossible to buy: {}'.format(possible_to_buy))      # up, +100 shares
+print('\nUp Trend: {}'.format(up_trend))                    # up, +100 shares, profit > 0
+print('\nSignal: {}'.format(signal))                        # up, MA10 > Price
 
 
 
 
-#for ind in selection:    
-#    
-#    quotes = Data.get(Data(index, start, end))    
-#    
-#    fig, ax = plt.subplots(figsize=(8,6))
-#    candlestick2_ohlc(ax, quotes['Open'], quotes['High'], quotes['Low'], quotes['Close'], width=0.6)
-#    plt.xticks(rotation = 45)
-#    plt.title('Stock Index {}'.format(index))
-#    plt.xlabel('Days in period [{} - {}]'.format(start, end))
-#    plt.ylabel('{} Price'.format(index))
-#    
-#    plt.show()
+option = input("\nPlot the selected Indexes chart ?\n\n 'y' for yes\n\n 'n' for no\n\n>>> ")
+while option not in ['y', 'n']:
+    option = input("\nPlot the selected Indexes chart ?\n\n 'y' for yes\n\n 'n' for no\n\n>>> ")
+
+if option == 'y':
+    for i in selection:
+    
+        class Data:    
+            def __init__(self, index, start, date):
+                self.index = i
+                self.start = start
+                self.date = date
+                
+            def get(self):
+                return web.DataReader(self.index, 'google', self.start, self.date)
+        
+        class rol_mean_plot:
+            def __init__(self, data, window):
+                self.data = data
+                self.window = window    
+               
+            def rol_mean(self):
+                return self.data.rolling(window=self.window).mean().plot(subplots=False, 
+                                        grid=True, figsize=(8, 6)) 
+                
+            def plot(y, window):
+                rol_mean_plot.rol_mean(rol_mean_plot(y, window))
+        
+          
+        if int(dt[1])-2 < 0:
+            month = 1
+        else:
+            month = int(dt[1])-2
+        quotes_total = Data.get(Data(i, datetime.datetime(int(dt[0]),month,int(dt[2])), end))
+        quotes = Data.get(Data(i, start, end))
+        
+    
+        quotes['MA20'] = quotes_total['Close'].rolling(20).mean()
+    
+        quotes['MA40'] = quotes_total['Close'].rolling(40).mean()    
+        
+        quotes['MA10'] = quotes_total['Close'].rolling(10).mean()     
+        
+        
+        quotes.reset_index(inplace=True)        
+        
+        fig, ax = plt.subplots(figsize=(10, 4))
+        plt.xticks(rotation = 45)
+        plt.title('Stock Index {}'.format(i))
+        plt.xlabel('Days in period [{} - {}]'.format(start, end))
+        plt.ylabel('{} Price'.format(i))
+        
+        candlestick2_ohlc(ax, quotes['Open'], quotes['High'], quotes['Low'], quotes['Close'], width=0.6)
+        quotes['MA20'].plot(ax=ax)
+        quotes['MA40'].plot(ax=ax)
+        quotes['MA10'].plot(ax=ax)
+        
+        
+        plt.legend('Rolling Mean 20 days', 'Rolling Mean 40 days', loc='upper left')    
+        plt.show()
+elif option == 'n':
+    pass
 
